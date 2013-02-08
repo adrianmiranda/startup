a272dfa78578ab8986abf043b4e1a5b9a160758a
Adrian Miranda <ad@adrianmiranda.com.br>
2013-02-08 07:01:44 -0200

    Update actionscript/publish/scripts/app/swfembed.js

--- actionscript/publish/scripts/app/swfembed.js
+++ actionscript/publish/scripts/app/swfembed.js
@@ -34,7 +34,7 @@
 			};
 
 			var settings = {
-				  express: flashvars.fvBaseContent+'noflash/expressInstall.swf'
+				  express: flashvars.fvBaseContent+'noflash/support/expressInstall.swf'
 				, swf: flashvars.fvBaseContent+(params.swf || 'boot.swf')
 				, version: params.version || '10.1.000'
 				, div: params.div || 'flash'
